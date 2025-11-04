81b072f5eb8baaad94236c5d097f57148e74d5be
Arya Adinata <aryalovesiesta@gmail.com>
2025-11-04 08:08:23 +0700

    Add files via upload

---
+++ operator_penugasan.py
@@ -0,0 +1,14 @@
+# Tabungan
+tabungan = float(input("Masukkan Jumlah Tabungan: "))
+
+# Pengeluaran hari pertama
+pengeluaran = float(input("Masukkan pengeluaran hari pertama: "))
+
+# Pengeluaran hari kedua
+pengeluaran += float(input("Masukkan pengeluaran hari kedua: "))
+
+# Tabungan dikurangi pengeluaran
+tabungan -= pengeluaran
+
+# Tampilkan total tabungan
+print("Total tabungan anda sekarang:", tabungan)
